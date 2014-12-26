8ab1670a6beb58e4c6a3e71871e30ea8bb8da8fb
Katelyn Gigante <silasary@users.noreply.github.com>
2014-12-26 21:39:14 +1100

    Fingbot!

---
+++ Fingbot/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Fingbot
+{
+    [DataContract]
+    class Settings
+    {
+        [DataMember]
+        public string Token { get; set; }
+
+        [DataMember]
+        public string Channel { get; set; }
+
+        [DataMember]
+        public string FingXml { get; set; }
+    }
+}

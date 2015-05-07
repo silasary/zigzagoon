72799eeffd75ce29fd7c4e29550bcac136ef6894
Katelyn Gigante <silasary@users.noreply.github.com>
2015-05-07 16:32:01 +1000

    Multiteam!

---
+++ Fingbot/Commands/HelpCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fingbot.Commands
+{
+    class HelpCommand : ICommand
+    {
+        bool ICommand.Run(string MessageText, SlackRTM.Events.Message RawMessage, bool IsTargeted, SlackRTM.Slack Instance)
+        {
+            return false;
+        }
+    }
+}

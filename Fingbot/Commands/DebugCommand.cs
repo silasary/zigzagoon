c5057a4ea896c84997b95cbc50768885fd17c92a
Katelyn Gigante <silasary@users.noreply.github.com>
2015-06-26 18:33:44 +1000

    Debug uses IsTargeted, just like every other command.

--- Fingbot/Commands/DebugCommand.cs
+++ Fingbot/Commands/DebugCommand.cs
@@ -16,18 +16,26 @@ namespace Fingbot.Commands
              * ****/
             var match = Regex.Match(
                 MessageText,
-                //TODO: Maybe?
-                string.Concat("@", Instance.Self.Name, @":?\s+Debug ?(?<online>online)?"),
+                
+                string.Concat("Debug ?((?<online>online)?|(?<mine>mine)|(?<unknown>unknown))"),
                 RegexOptions.IgnoreCase);
+            if (!IsTargeted)
+                return false; // No inform here.
             if (match.Success)
             {
                 bool online = !string.IsNullOrEmpty(match.Groups["online"].Value);
+                bool mine = !string.IsNullOrEmpty(match.Groups["mine"].Value);
+                bool unknown = !string.IsNullOrEmpty(match.Groups["online"].Value);
                 int n = 0;
                 var sb = new StringBuilder();
                 NetworkData network = Singleton<NetworkData>.Instance;
                 foreach (var host in network.AllHosts)
                 {
                     if (online && host.State == "down")
+                        continue;
+                    if (mine && host.Owner != RawMessage.User) // TODO: Utterly broken - Not the same.
+                        continue;
+                    if (unknown && !string.IsNullOrEmpty(host.Owner))
                         continue;
                     sb.AppendFormat("{0}: {1} ({2})", host.FriendlyName, network.Status(host), host.Age).AppendLine();
                     if (n++ == 10)

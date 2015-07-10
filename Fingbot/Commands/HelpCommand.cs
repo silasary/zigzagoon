c059d4221aa123c83f3caf5d3b6969544bfd2a58
Katelyn Gigante <silasary@users.noreply.github.com>
2015-07-10 17:29:32 +1000

    Introduce yourself!

--- Fingbot/Commands/HelpCommand.cs
+++ Fingbot/Commands/HelpCommand.cs
@@ -2,14 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using Kamahl.Common;
+using SlackRTM;
+using SlackRTM.Events;
 
 namespace Fingbot.Commands
 {
     class HelpCommand : ICommand
     {
-        bool ICommand.Run(string MessageText, SlackRTM.Events.Message RawMessage, bool IsTargeted, SlackRTM.Slack Instance)
+        bool ICommand.Run(string MessageText, Message RawMessage, bool IsTargeted, Slack Instance)
         {
+            var match = Regex.Match(
+               MessageText,
+               @"Introduce yourself",
+               RegexOptions.IgnoreCase);
+            if (match.Success && IsTargeted)
+                Introduce(Instance, Instance.GetChannel(RawMessage.Channel));
             return false;
+        }
+
+        public static void Introduce(Slack instance, Channel channel)
+        {
+            var settings = PersistentSingleton<Settings>.Instance;
+
+            instance.SendMessage(channel, "Hi, I'm {0}!", instance.Self.Name);
+            instance.SendMessage(channel,
+                "I'm here to keep an eye on your building while you're not around.\n" +
+                "I do this by watching your wifi network. " +
+                "I'll keep notes on each device that connects to the wifi, and with your help, be able to work out who's in the building.\n");
+            instance.SendMessage(channel, "If you ever need me, just ask _Who's in?_ or _Anyone in?_, and I'll let you know.\n" +
+                "If there's a device I don't recognise, just say _@{0}: That's my iThing_, or _@{0}: That's @somebody's bright pink phone_.\n" +
+                "I try to work out what belongs here, and try not to ask about the router or printer, but if I ever do, feel free to let me know by saying _@{0}: Ignore that_.", instance.Self.Name);
+            settings.HasDoneIntroSpiel = true;
         }
     }
 }
--- Fingbot/Program.cs
+++ Fingbot/Program.cs
@@ -161,15 +161,7 @@ namespace Fingbot
                     if (!settings.HasDoneIntroSpiel)
                     {
                         var channel = instance.PrimaryChannel.IsMember ? instance.PrimaryChannel : instance.JoinedChannels.FirstOrDefault();
-                        instance.SendMessage(channel, "Hi, I'm {0}!", instance.Self.Name);
-                        instance.SendMessage(channel,
-                            "I'm here to keep an eye on your building while you're not around.\n" +
-                            "I do this by watching your wifi network. " +
-                            "I'll keep notes on each device that connects to the wifi, and with your help, be able to work out who's in the building.\n");
-                        instance.SendMessage(channel, "If you ever need me, just ask _Who's in?_ or _Anyone in?_, and I'll let you know.\n" +
-                            "If there's a device I don't recognise, just say _@{0}: That's my iThing_, or _@{0}: That's @somebody's bright pink phone_.\n" +
-                            "I try to work out what belongs here, and try not to ask about the router or printer, but if I ever do, feel free to let me know by saying _@{0}: Ignore that_.", instance.Self.Name);
-                        settings.HasDoneIntroSpiel = true;
+                        HelpCommand.Introduce(instance, channel);
                         PersistentSingleton<Settings>.Dirty();
                     }
                 }
@@ -217,6 +209,8 @@ namespace Fingbot
                 Console.WriteLine("!An Error has been caught!\n{0}", c);
             }
         }
+
+        
 
 
         private static string SubstituteMarkup(string p, Slack instance)

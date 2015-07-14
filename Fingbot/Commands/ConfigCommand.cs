341fe8ed57e8cc7c5e442930e4fa184be9238868
Katelyn Gigante <clockwork.singularity@gmail.com>
2015-07-14 17:23:35 +1000

    And start it

---
+++ Fingbot/Commands/ConfigCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using SlackRTM;
+using SlackRTM.Events;
+
+namespace Fingbot.Commands
+{
+    class ConfigCommand : ICommand
+    {
+        public bool Run(string MessageText, Message RawMessage, bool IsTargeted, Slack Instance)
+        {
+            var match = Regex.Match(
+                    MessageText,
+                    @".config (?<setting>[\w:]+)",
+                    RegexOptions.IgnoreCase);
+            
+            return false;
+        }
+    }
+}
